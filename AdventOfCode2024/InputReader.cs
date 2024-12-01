b14b05e9baba8d9d54d71e7be0cc53918cf4e6d7
Adam <adam.daniluk@gmail.com>
2024-12-01 19:14:00 +0100

    Day 1

---
+++ AdventOfCode2024/InputReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace AdventOfCode2024;
+
+public static class InputReader
+{
+    public static IList<string> GetInputList(string fileName)
+    {
+        var path = Path.Combine(Environment.CurrentDirectory, @"Input/", fileName + ".txt");
+        var list = new List<string>();
+
+        using var streamReader = new StreamReader(path, Encoding.UTF8);
+        while (!streamReader.EndOfStream)
+        {
+            list.Add(streamReader.ReadLine()!);
+        }
+
+        return list;
+    }
+}
---
+++ AdventOfCode2024/Solutions/Day1.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+using static System.Int32;
+
+namespace AdventOfCode2024.Solutions;
+
+public class Day1
+{
+    private static readonly IList<string> InputList = InputReader.GetInputList(nameof(Day1));
+
+    private readonly List<int> _leftListOrdered;
+    private readonly List<int> _rightListOrdered;
+    
+    public Day1()
+    {
+        var leftList = new List<int>();
+        var rightList = new List<int>();
+        
+        Regex reg = new("\\d+");
+        foreach (var line in InputList)
+        {
+            var matches = reg.Matches(line);
+            leftList.Add(Parse(matches[0].Value));
+            rightList.Add(Parse(matches[1].Value));
+        }
+
+        _leftListOrdered = leftList.Order().ToList();
+        _rightListOrdered = rightList.Order().ToList();
+    }
+    
+    public int Task1()
+    {
+        var result = 0;
+
+        for (int i = 0; i < _leftListOrdered.Count; i++)
+        {
+            result += Math.Abs(_leftListOrdered[i] - _rightListOrdered[i]);
+        }
+        
+        return result;
+    }
+
+    public int Task2()
+    {
+        var result = 0;
+
+        foreach (var number in _leftListOrdered)
+        {
+            result += _rightListOrdered.Count(x => x == number) * number;
+        }
+
+        return result;
+    }
+}
